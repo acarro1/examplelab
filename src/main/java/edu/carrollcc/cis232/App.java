13eafb3e5b0aa1797ecf8ea3583de1f1b7a90073
Alex Carroll <acarro1@umbc.edu>
2016-02-01 20:45:57 -0500

    My solution for the example lab. Did the thing.

--- src/main/java/edu/carrollcc/cis232/App.java
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -1,13 +1,14 @@
 package edu.carrollcc.cis232;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+	public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+    	Scanner keyboard = new Scanner(System.in);
+    	System.out.println("What is your name?");
+    	String Name = keyboard.nextLine();
+        System.out.printf("Hello %s!" , Name);
+    	keyboard.close();
     }
 }
